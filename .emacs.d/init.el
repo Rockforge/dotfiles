e36f8f6f5ba80c4afdd3de8e997ebac64536fe0f
Chan <25656081+Rockforge@users.noreply.github.com>
2024-03-05 15:47:15 +0800

    Update files

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,6 +20,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("88cb0f9c0c11dbb4c26a628d35eb9239d1cf580cfd28e332e654e7f58b4e721b" "9f1d0627e756e58e0263fe3f00b16d8f7b2aca0882faacdc20ddd56a95acb7c2" "7397cc72938446348521d8061d3f2e288165f65a2dbb6366bb666224de2629bb" default))
  '(vue-html-extra-indent 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
